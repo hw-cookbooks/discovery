e92eecd9843ee879be5d1f583b82cd1baabf1ae0
AJ Christensen <aj@junglist.gen.nz>
2012-06-14 11:36:01 +1200

    [search] [all] backwards compatibility for search, wrap all method

--- libraries/search.rb
+++ libraries/search.rb
@@ -5,44 +5,9 @@ require "chef/log"
 module Discovery
   class << self
     def search( role = "", options = {})
-      raise ArgumentError.new("You must pass a role") if role.empty?
-      raise ArgumentError.new("Options must be a hash") unless options.respond_to? :has_key?
-      raise ArgumentError.new("Options does not contain a node key") unless options.has_key? :node
-
-      options[:environment_aware] ||= false
-
-      Chef::Log.debug "discovery: doing enviornment aware search" if options.has_key? :environment_aware and options[:environment_aware]
-
-      results = []
-
-      case options.has_key? :environment_aware and options[:environment_aware]
-      when true
-        [ "chef_environment:#{options[:node].chef_environment} AND roles:#{role}",
-          "chef_environment:#{options[:node].chef_environment} AND role:#{role}" ]
-      when false
-        [ "roles:#{role}",
-          "role:#{role}" ]
-      end.detect do |search|
-        results = _query(search)
-        next if results.empty?
-        results
-      end
-
-      if results.empty?
-        if options[:node].run_list.include? "role[#{role}]" or options[:node].roles.include? role
-          Chef::Log.debug "discovery: empty results and local node includes role #{role}, falling back to local"
-          return options[:node]
-        else
-          Chef::Log.debug "discovery: node run_list: #{options[:node].run_list.inspect}, roles: #{options[:node].roles.inspect}"
-          raise RuntimeError.new("No response from search and no local node did not include #{role}") if results.empty?
-        end
-      end
-
-      if results.first == options[:node]
-        Chef::Log.warn "discovery: search response is ourself, connecting to localhost"
-      end
-
-      return results.first
+      # All returns all of the nodes, they're already sorted by
+      # ohai_time, so grab the first one.
+      all(role, options).first
     end
 
     def all( role = "", options = {})
