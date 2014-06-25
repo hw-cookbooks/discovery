a6feb59e089a0e395d96ab7616287c04a8070c2e
Sean Porter <portertech@gmail.com>
2014-06-25 14:37:11 -0500

    [label] test disco lib ipaddress, prepare for label support, backfill'n like a vill'n

--- libraries/ipaddress.rb
+++ libraries/ipaddress.rb
@@ -12,6 +12,7 @@ module Discovery
     end
 
     def region_for_ec2_node(node)
+      raise "Must pass a node" unless node
       if node.has_key? :ec2 and
           node.ec2.has_key? :placement_availability_zone
         node[:ec2][:placement_availability_zone].gsub(/(\d+).+/,'\1')
@@ -28,9 +29,8 @@ module Discovery
       raise "Options does not contain a remote_node key" unless
         options.has_key? :remote_node
       raise "Options type is invalid" if
-        options.has_key? :type and
-        options[:type].is_a? Symbol and not
-        [:local, :public].any? { |o| o == options[:type] }
+        options.has_key? :type and not
+          [:local, :public].include?(options[:type])
 
       options[:type] ||=
         if provider_for_node(options[:remote_node]) == provider_for_node(options[:node])
@@ -49,27 +49,8 @@ module Discovery
 
       Chef::Log.debug "ipaddress[#{options[:type]}]: attempting to determine ip address for #{options[:node].name}"
 
-      [(begin
-          if options[:remote_node].has_key? :cloud
-            options[:remote_node].cloud.send("#{options[:type]}_ipv4")
-          else
-            nil
-          end
-        rescue ArgumentError
-          nil
-        end),
-        options[:remote_node].ipaddress
-      ].detect do |attribute|
-        begin
-          attribute
-        rescue StandardError => standard_error
-          Chef::Log.debug "ipaddress: error #{standard_error}"
-          nil
-        rescue Exception => exception
-          Chef::Log.debug "ipaddress: exception #{exception}"
-          nil
-        end
-      end
+      cloud_ipv4 = options[:remote_node][:cloud]["#{options[:type]}_ipv4"] rescue nil
+      cloud_ipv4 || options[:remote_node][:ipaddress]
     end
 
   end
